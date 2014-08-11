664124524616ee15a8017b19eb169c00dbb3430c
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-08-10 22:25:37 -0400

    Improved custom plugin handling

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,29 +1,5 @@
 $(document).ready(function() {
-    $('hidden-img').each(function(i, himg) {
-        $(himg).replaceWith(function() {
-            var butt = document.createElement('button');
-            $(butt)
-                .attr('type', 'button')
-                .data('src', $(himg).attr('src'))
-                .text('Show Image')
-                .on('click', function() {
-                    $(this).hide();       
-                    var old = $('#hidden-image-' + i);
-                    if(old.length > 0) {
-                        old.show();
-                    } else {
-                        var img = document.createElement('img');
-                        $(img)
-                            .attr('src', $(this).data('src'))
-                            .attr('id', 'hidden-image-' + i)
-                            .on('click', function() {
-                                $(this).hide();
-                                $(butt).show();
-                            });
-                        $(img).insertAfter($(this));
-                    }
-                });
-            return butt;
-        });
+    PLUGINS.forEach(function(plugin) {
+        plugin.apply();
     });
 });
---
+++ assets/js/plugins.js
@@ -0,0 +1,34 @@
+function HiddenImages() {
+    this.apply = function() {
+        $('hidden-img').each(function(i, himg) {
+            $(himg).replaceWith(function() {
+                var butt = document.createElement('button');
+                $(butt)
+                    .attr('type', 'button')
+                    .data('src', $(himg).attr('src'))
+                    .text('Show Image')
+                    .on('click', function() {
+                        $(this).hide();       
+                        var old = $('#hidden-image-' + i);
+                        if(old.length > 0) {
+                            old.show();
+                        } else {
+                            var img = document.createElement('img');
+                            $(img)
+                                .attr('src', $(this).data('src'))
+                                .attr('id', 'hidden-image-' + i)
+                                .on('click', function() {
+                                    $(this).hide();
+                                    $(butt).show();
+                                });
+                            $(img).insertAfter($(this));
+                        }
+                    });
+                return butt;
+            });
+        });
+    }
+}
+
+
+PLUGINS = [new HiddenImages()];
